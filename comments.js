796265a9fe11a74653622aa76224b4b5bc0f9c47
Gabriel Gustavo Adolfo Escobar Ugarte <mistergge@gmail.com>
2024-06-16 01:13:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,21 @@
-// Create a web server
-// Load the http module
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+// Create web server
+// 1. Load the http module
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// Configure our HTTP server to respond with Hello World to
+// 2. Create a web server
+http.createServer((req, res) => {
+    // 3. Get the file name
+    let fileName = path.basename(req.url);
+    console.log(fileName);
+    // 4. Read the file
+    fs.readFile( fileName, (err, data) => {
+        // 5. Write the file
+        res.write(data);
+        res.end();
+    }
+    );
+}
+).listen(8080);
+console.log('Server is running at http://localhost:8080');
