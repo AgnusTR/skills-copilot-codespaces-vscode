6d5f2e2163e214cd192d77a285f41addd8e2df88
Gabriel Gustavo Adolfo Escobar Ugarte <mistergge@gmail.com>
2024-06-16 01:03:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        details: {
+            country: "US",
+            city: "Boston",
+            salary: 70000
+        }
+    };
+    return member;
+}
